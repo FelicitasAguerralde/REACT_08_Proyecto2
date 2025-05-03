0e7a30a32518c0accdf797b7ec4d9e994df86b2c
Felicitas <felicitas.aguerralde@gmail.com>
2025-05-03 12:20:00 -0300

    Agrego ruteo

---
+++ src/router/AppRouter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Index } from '../components/Index';
+import { Portfolio } from '../components/Portfolio';
+import { Services } from '../components/Services';
+import { Curriculum } from '../components/Curriculum';
+import { Contact } from '../components/Contact';
+
+
+export const AppRouter = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+        <Route path='/' element={<Index />} />
+        <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/curriculum' element={<Curriculum />} />
+        <Route path='/contact' element={<Contact />} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
